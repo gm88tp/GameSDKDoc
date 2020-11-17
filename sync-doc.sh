82fe3ec265ccc667903fa2110083a33f099ebd7f
流水寿司 <charlestide@vip.163.com>
2020-11-17 10:41:29 +0800

    增加docs目录，将子模块中的文档复制出来

---
+++ sync-doc.sh
@@ -0,0 +1,10 @@
+# shellcheck disable=SC2225
+cp Android/Domestic/docs/DOCUMENT.md docs/Android-Domestic.md
+cp Android/Global/docs/DOCUMENT.md docs/Android-Global.md
+cp Android/Global-StandAlone/docs/DOCUMENT.md docs/Android-Global-StandAlone.md
+cp Ios/Domestic/docs/DOCUMENT.md docs/Ios-Domestic.md
+cp Ios/Global/docs/DOCUMENT.md docs/Ios-Global.md
+cp Ios/Global-StandAlone/docs/DOCUMENT.md docs/Ios-Global-StandAlone.md
+cp Server/docs/Domestic/DOCUMENT.md docs/Server-Domestic.md
+cp Server/docs/Global/DOCUMENT.md docs/Server-Global.md
+
