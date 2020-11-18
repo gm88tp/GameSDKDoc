5cead1180d968683a046d06377fbb34df1355c63
流水寿司 <charlestide@vip.163.com>
2020-11-18 11:56:33 +0800

    增加白鹭文档

--- sync-doc.sh
+++ sync-doc.sh
@@ -4,11 +4,13 @@ cp Android/Global/docs/DOCUMENT.md docs/Android-Global.md
 cp Android/Global-StandAlone/docs/DOCUMENT.md docs/Android-Global-StandAlone.md
 cp Ios/Domestic/docs/DOCUMENT.md docs/Ios-Domestic.md
 cp Ios/Global/docs/DOCUMENT.md docs/Ios-Global.md
+cp Ios/Global/docs/egretDoc.md docs/Egret-Global.md # 白鹭
 cp Ios/Global-StandAlone/docs/DOCUMENT.md docs/Ios-Global-StandAlone.md
 cp Server/docs/Domestic/DOCUMENT.md docs/Server-Domestic.md
 cp Server/docs/Domestic/H5-DOCUMENT.md docs/Server-H5-Domestic.md
 cp Server/docs/Global/DOCUMENT.md docs/Server-Global.md
 cp Server/docs/Global/H5-DOCUMENT.md docs/Server-H5-Global.md
+
 
 # 图片
 cp Ios/Global/docs/assets/images/* docs/assets/images
